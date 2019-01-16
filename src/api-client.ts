08bc0d522f04ecf17d80916c5320671e137f857b
kbridbur <kbridbur@mit.edu>
2019-01-16 12:29:03 -0800

    Change to axios, add instructions for testing package to api-client.ts

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,8 +1,16 @@
-const http = require('http');
+const axios = require('axios');
 
 class RevAiApiClient {
     apiKey: string;
     constructor(apiKey: string) {
         this.apiKey = apiKey;
     }
+
+    test(): string {
+        return this.apiKey; 
+    }
 }
+
+// Uncomment this, compile the typescript and run "node ./dist/api-client.ts" to test changes
+// const client = new RevAiApiClient("im a key");
+// console.log(client.test());
