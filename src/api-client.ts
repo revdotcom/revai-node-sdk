ce146bd6bb56902e0b3ff1fefbbddee77b8da65a
celine <celqiu@gmail.com>
2019-01-29 11:31:30 -0800

    add basic account and job methods

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,16 +1,79 @@
-const axios = require('axios');
+import axios, { AxiosInstance } from 'axios';
 
 class RevAiApiClient {
-    apiKey: string;
-    constructor(apiKey: string) {
-        this.apiKey = apiKey;
+    accessToken: string;
+    version: string;
+    instance: AxiosInstance;
+    constructor(accessToken: string, version = 'v1') {
+        this.accessToken = accessToken;
+        axios.defaults.baseURL = `https://api.rev.ai/revspeech/${version}/`;
+        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
     }
 
-    test(): string {
-        return this.apiKey; 
+    async getAccount(): Promise<RevAiAccount> {
+        try {
+            const response = await axios.get('account');
+            const account = response.data;
+            return account;
+        }
+        catch (error) {
+            console.log('error: ', error.response.data);
+        }
+    }
+
+    async getJobDetails(id: string): Promise<RevAiApiJob> {
+        try {
+            const response = await axios.get(`/jobs/${id}`);
+            const job = response.data;
+            return job;
+        }
+        catch (error) { 
+            console.log('error: ', error.response.data);
+        }
+    }
+
+    async submitJobUrl(mediaUrl: string, options?: RevAiJobOptions): Promise<RevAiApiJob> {
+        if (options)
+            options['media_url'] = mediaUrl;
+        else
+            options = { 'media_url': mediaUrl };
+        try {
+            const response = await axios.post('/jobs', options, {
+                headers: { 'Content-Type': 'application/json' }
+            });
+            const job = response.data;
+            return job;
+        }
+        catch (error) {
+            console.log('error: ', error.response.data);
+        }
     }
 }
 
-// Uncomment this, compile the typescript and run "node ./dist/api-client.ts" to test changes
-// const client = new RevAiApiClient("im a key");
-// console.log(client.test());
+export default RevAiApiClient;
+
+export interface RevAiJobOptions {
+    media_url?: string;
+    metadata?: string;
+    callback_url?: string;
+    skip_diarization?: boolean;
+}
+
+export interface RevAiAccount {
+    email: string;
+    balance_seconds: number;
+}
+
+export interface RevAiApiJob {
+    id: string;
+    status: string;
+    created_on: string;
+    completed_on?: string;
+    metadata?: string;
+    name?: string;
+    callback_url?: string;
+    duration_seconds?: number;
+    media_url?: string;
+    failure?: string;
+    failure_detail?: string;
+}
