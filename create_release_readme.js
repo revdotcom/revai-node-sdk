b2c9c3d7efce3bc9a702bfb1ebeac4ce2b63786c
kbridbur <kbridbur@mit.edu>
2019-03-01 20:54:54 -0800

    Create readme in release package

---
+++ create_release_readme.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+let filename = 'README.md';
+let src = path.join(__dirname, filename);
+let destDir = path.join(__dirname, 'dist');
+
+fs.access(destDir, (err) => {
+  if(err)
+    fs.mkdirSync(destDir);
+
+  copyFile(src, path.join(destDir, filename));
+});
+
+
+function copyFile(src, dest) {
+
+  let readStream = fs.createReadStream(src);
+
+  readStream.once('error', (err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
+  readStream.once('end', () => {
+    console.log('done copying');
+    process.exit(0);
+  });
+
+  readStream.pipe(fs.createWriteStream(dest));
+}
