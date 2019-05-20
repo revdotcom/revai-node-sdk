345c132a9bab05dd9359ad1976804b8d5ae673e6
kbridbur <kbridbur@mit.edu>
2019-05-20 14:36:21 -0700

    Add events for clinet

--- src/streaming-client.ts
+++ src/streaming-client.ts
@@ -1,12 +1,13 @@
 import * as fs from 'fs';
 import * as sockets from 'websocket';
+import * as events from 'events';
 import { Duplex, PassThrough } from 'stream';
 
 import AudioConfig from './models/streaming/AudioConfig';
 import BufferedDuplex from './models/streaming/BufferedDuplex';
 import StreamingResponse from './models/streaming/StreamingResponse';
 
-export class RevAiStreamingClient {
+export class RevAiStreamingClient extends events.EventEmitter {
     accessToken: string;
     config: AudioConfig;
     baseUrl: string;
@@ -14,9 +15,10 @@ export class RevAiStreamingClient {
     responses: PassThrough;
     client: sockets.WebSocketClient;
     constructor(accessToken: string, config: AudioConfig, version = 'v1alpha'){
+        super();
         this.accessToken = accessToken;
         this.config = config;
-        this.baseUrl = `wss://api.rev.ai/speechtotext/${version}/stream?` +
+        this.baseUrl = `wss://api-test.rev.ai/speechtotext/${version}/stream?` +
             `access_token=${accessToken}` +
             `&content_type=${config.getContentTypeString()}`;
         this.requests = new PassThrough();
@@ -27,19 +29,20 @@ export class RevAiStreamingClient {
     start (): Duplex {
         var self = this;
         this.client.on('connectFailed', function(error: Error) {
-            console.log("Failed to connect to streaming service");
+            self.emit('connectFailed', error);
             self.requests.end();
             throw error;
         });
         this.client.on('connect', function(connection: any) {
+            self.emit('connect');
             connection.on('error', function(error : Error) {
-                console.log("Error thrown in connection to streaming service");
+                self.emit('error', error);
                 self.requests.end();
                 self.responses.push(null);
                 throw error;
             });
             connection.on('close', function(code: number, reason: string) {
-                console.log(`Connection closed, ${code}: ${reason}`);
+                self.emit('close', code, reason);
                 self.requests.end();
                 self.responses.push(null);
             });
@@ -52,7 +55,7 @@ export class RevAiStreamingClient {
             function sendFromBuffer() {
                 if (connection.connected) {
                     var value = self.requests.read(self.requests.readableLength);
-                    if (value !== null) {
+                    if (value != null) {
                         connection.send(value);
                     }
                     setTimeout(sendFromBuffer, 100);
@@ -61,6 +64,7 @@ export class RevAiStreamingClient {
             sendFromBuffer();
         });
         this.client.connect(this.baseUrl);
+        console.log(this.client);
         return new BufferedDuplex(this.requests, this.responses, { readableObjectMode: true });
     }
 
@@ -79,6 +83,13 @@ audioConfig.contentType = "audio/x-wav";
 const token = "token";
 var client = new RevAiStreamingClient(token, audioConfig);
 var stream = client.start();
+client.on('connect', () => {
+    console.log("client connected");
+});
+client.on('close', (code, reason) => {
+    console.log(`Connection closed, ${code}: ${reason}`);
+});
+
 var file = fs.createReadStream('../test.wav');
 file.pipe(stream);
 stream.on('data', data => {
