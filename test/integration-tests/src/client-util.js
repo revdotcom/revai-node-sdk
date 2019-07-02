b045306f16ea6a31eaf0d39fb3e365010d2dc504
Chanc3 <chanceshiflett@yahoo.com>
2019-07-02 09:18:57 -0700

    reformat

--- test/integration-tests/src/client-util.js
+++ test/integration-tests/src/client-util.js
@@ -1,11 +1,10 @@
 const configUtil = require('./config-util');
 const revai = require('revai-node-sdk');
 
-
 module.exports = {
     getClient: function (apiKey) {
         let client = new revai.RevAiApiClient(apiKey);
         client.apiHandler.instance.defaults.baseURL = configUtil.getBaseUrl();
         return client;
     }
-};
+}
