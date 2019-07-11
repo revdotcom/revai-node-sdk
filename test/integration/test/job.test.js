ee052a8f8697673a4ae447acbca20f98da6e56e2
Chanc3 <chanceshiflett@yahoo.com>
2019-07-10 20:34:48 -0700

    add job submission tests

---
+++ test/integration/test/job.test.js
@@ -0,0 +1,20 @@
+const clientHelper = require('../src/client-helper');
+const configHelper = require('../src/config-helper');
+
+test('Can submit local file', async () => {
+    const client = clientHelper.getClient(configHelper.getApiKey());
+    const options = new Object();
+    options.metadata = 'Node sdk submit local file';
+    const job = await client.submitJobLocalFile('./resources/test_mp3.mp3', options);
+    expect(job.status).toBe('in_progress');
+    expect(job.id).not.toBeNull();
+});
+
+test('Can submit url', async () => {
+    const client = clientHelper.getClient(configHelper.getApiKey());
+    const options = new Object();
+    options.metadata = 'Node sdk submit url';
+    const job = await client.submitJobUrl('https://www.rev.ai/FTC_Sample_1.mp3');
+    expect(job.status).toBe('in_progress');
+    expect(job.id).not.toBeNull();
+});
