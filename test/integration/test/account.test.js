19bb2814bf315f06e30da9a07dc7add68cad4831
Chanc3 <chanceshiflett@yahoo.com>
2019-07-10 20:35:23 -0700

    refactor and add authentication test

--- test/integration/test/account.test.js
+++ test/integration/test/account.test.js
@@ -1,9 +1,22 @@
-const clientUtil = require('../src/client-helper');
-const configUtil = require('../src/config-helper');
+const clientHelper = require('../src/client-helper');
+const configHelper = require('../src/config-helper');
 
-test('Should return email and balance', async () => {
-    const client = clientUtil.getClient(configUtil.getApiKey());
+test('Can return email and balance', async () => {
+    const client = clientHelper.getClient(configHelper.getApiKey());
     const account = await client.getAccount();
-    expect(account.email).toBe(configUtil.getUserEmail());
+    expect(account.email).toBe(configHelper.getUserEmail());
     expect(account.balance_seconds).not.toBeNull();
 });
+
+test('Cannot authenticate with fake token', async () => {
+    const randomString = Math.random().toString(36).replace('0.', '');
+    const client = clientHelper.getClient(randomString);
+    try {
+        await client.getAccount();
+    } catch (error) {
+        var revAiError = error;
+    } finally {
+        expect(revAiError.statusCode).toEqual(401);
+        expect(revAiError.title).toBe('Authorization has been denied for this request.');
+    }
+})
