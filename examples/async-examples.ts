90e97fa41e3cccfbe02305287ec99884491e7678
Albert Menio <albert.menio@rev.com>
2019-07-15 16:55:02 -0700

    Responded to PR feedback

--- examples/async-examples.ts
+++ examples/async-examples.ts
@@ -1,47 +1,53 @@
+import {RevAiApiClient, RevAiJobOptions} from 'revai-node-sdk';
+
 /* Initialize your client with your revai access token */
 const token = <YOUR-ACCESS-TOKEN>;
 var client = new RevAiApiClient(token);
 
 /* Get account details */
 var account = await client.getAccount();
-console.log(`Account: {account.email}`);
-console.log(`Balance: {account.balance_seconds} seconds`);
+console.log(`Account: ${account.email}`);
+console.log(`Balance: ${account.balance_seconds} seconds`);
 
-/* Submit job and receive job object */
-var job = await client.submitJobUrl("https://www.rev.ai/FTC_Sample_1.mp3");
-// var job = client.submitJobLocalFile("./path/to/file.mp4");
+/* Submit job and receive job object
+ * Media may be submitted from a local file or from a url by using:
+ * client.submitJobUrl("https://www.rev.ai/FTC_Sample_1.mp3");
+ */
+var jobOptions = new RevAiJobOptions(
+	metadata = "This is an example",
+	skip_diarization = true
+	);
+var job = client.submitJobLocalFile("./example.raw");
 
-console.log(`Job Id: {job.id}`);
-console.log(`Status: {job.status}`);
-console.log(`Created On: {job.created_on}`);
+console.log(`Job Id: ${job.id}`);
+console.log(`Status: ${job.status}`);
+console.log(`Created On: ${job.created_on}`);
 
 /* Check the status of job */
 var jobDetails = await client.getJobDetails(job.id);
+console.log(`Status for job ${job.id} is ${jobDetails.status}`);
 
-/* Get all transcription jobs */
-var listOfJobs = await client.getListOfJobs();
+/* Get # of jobs started after a certain job id
+ * client.getListOfJobs() will default to returning all jobs
+ */
+var listOfJobs = await client.getListOfJobs(5, job.id);
 
-/* Get at most # of jobs */
-var listOfJobs = await client.getListOfJobs(5);
+/* Get transcript as plain text
+ * Transcripts can also be gotten as Object, Text Stream, or Object Stream via
+ * client.getTranscript{Object, TextStream, ObjectStream}() respectively
+ */
+while((await client.getJobDetails(job.id)).status == "in_progress")
+{
+	// Waits 5 seconds between each check
+	await new Promise( resolve => setTimeout(resolve, 5000));
+}
 
-/* Get all jobs started after a certain job id */
-var listOfJobs = await client.getListOfJobs(undefined, job.id);
-
-/* Get transcript as plain text */
 var transcriptText = await client.getTranscriptText(job.id);
+console.log(transcriptText);
 
-/* Get transcript as an object */
-var transcriptObject = await client.getTranscriptObject(job.id);
-
-/* Get transcript as text stream */
-var textStream = await client.getTranscriptTextStream(job.id);
-
-/* Get transcript as object stream */
-var transcriptStream = await client.getTranscriptObjectStream(job.id);
-
-/* Get captions */
+/* Transcripts may also be received as captions */
 var captionsStream = await client.getCaptions(job.id);
 
 /* Delete a job */
-await client.deleteJob(job.id);
+// await client.deleteJob(job.id);
 
--- examples/streaming-examples.ts
+++ examples/streaming-examples.ts
@@ -1,5 +1,14 @@
+import {AudioConfig, RevAiStreamingClient} from 'revai-node-sdk';
+import * as fs from 'fs';
+
 /* Initialize your client with your audio configuration and access token */
-const audioConfig = new AudioConfig(<YOUR-CONTENT-TYPE-PARAMS>);
+const audioConfig = new AudioConfig(
+	/* contentType */ "audio/raw", 
+	/* layout */      "interleaved", 
+	/* sample rate */ 16000,
+	/* format */      "S16LE",
+    /* channels */    1
+);
 const token = <YOUR-ACCESS-TOKEN>;
 var client = new RevAiStreamingClient(token, audioConfig);
 
@@ -8,20 +17,20 @@ client.on('close', (code, reason) => {
 	console.log(`Connection closed, ${code}: ${reason}`);
 });
 client.on('httpResponse', code => {
-	console.log(code);
+	console.log(`Streaming client received http response with code: ${code}`);
 })
 client.on('connectFailed', error => {
-	console.log(error);
+	console.log(`Connection failed with error: ${error}`);
 })
 client.on('connect', connectionMessage => {
-	console.log(connectionMessage);
+	console.log(`Connected with message: ${connectionMessage}`);
 })
 
 /* Begin streaming session */
 var stream = client.start();
 
 /* Establish file for streaming and streaming responses */
-var file = fs.createReadStream(<YOUR-AUDIO-FILE>);
+var file = fs.createReadStream("./example.raw");
 stream.on('data', data => {
 	console.log(data);
 });
