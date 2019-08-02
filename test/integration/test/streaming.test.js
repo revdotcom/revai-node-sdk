bf7ad700e145e71920e5f3a9dfbbac1da462a860
Chanc3 <chanceshiflett@yahoo.com>
2019-08-02 13:20:06 -0700

    streaming method variations

--- test/integration/test/streaming.test.js
+++ test/integration/test/streaming.test.js
@@ -4,9 +4,10 @@ const configHelper = require('../src/config-helper');
 const fs = require('fs');
 
 test('Can stream raw audio file', async (done) => {
-    jest.setTimeout(60000);
+    //jest.setTimeout(120000);
     const audioConfig = new AudioConfig("audio/x-raw", 'interleaved', 16000, 'S16LE', 1);
     const client = new RevAiStreamingClient(configHelper.getApiKey(), audioConfig);
+    client.baseUrl = 'wss://api-test.rev.ai/speechtotext/v1alpha/stream';
         
     client.on('close', (code, reason) => {
         console.log(`Connection closed, ${code}: ${reason}`);
@@ -21,21 +22,37 @@ test('Can stream raw audio file', async (done) => {
         console.log(connectMessage);
     });
 
+    console.log('BaseUrl: ' + client.baseUrl.toString());
+
     var stream = client.start();
 
-    var file = fs.createReadStream('./resources/english_test.raw');
     stream.on('data', data => {
-        console.log(data.toString());
-    });
-        
-    stream.on('finish', () => {
-        console.log("Finish event triggered");
+        console.log(JSON.stringify(data, null, 2));
     });
 
     stream.on('end', () => {
-        console.log('End event triggered')
-        client.end();
+        console.log('Stream end event called');
     })
+
+    // var bufs = [];
+    var file = fs.createReadStream('./resources/english_test.raw');
+
+    // file.once('end', () => {
+    //     console.log('File stream end event was triggered');
+    // });
+
+    // file.on('data', (chunk) => {
+    //     bufs.push(chunk);
+    // })
+
+    // file.on('end', () => {
+    //     var buf = Buffer.concat(bufs);
+    //     stream.write(buf);
+    // })
+
+    file.on('end', () => {
+        client.end();
+    });
 
     file.pipe(stream);
     
