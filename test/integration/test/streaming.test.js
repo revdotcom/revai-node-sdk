d9fcbbc49342a9733600ed1e65c753cadd62085b
Chance Shiflett <chanceshiflett@yahoo.com>
2019-08-15 10:51:51 -0700

    Add highWaterMark to readstream and use pipe

--- test/integration/test/streaming.test.js
+++ test/integration/test/streaming.test.js
@@ -3,14 +3,17 @@ const {RevAiStreamingClient} = require('../../../dist/src/streaming-client');
 const configHelper = require('../src/config-helper');
 const fs = require('fs');
 
-test('Can stream raw audio file', async (done) => {
-    //jest.setTimeout(120000);
+test('Can stream raw audio file', done => {
+    jest.setTimeout(60000);
     const audioConfig = new AudioConfig("audio/x-raw", 'interleaved', 16000, 'S16LE', 1);
     const client = new RevAiStreamingClient(configHelper.getApiKey(), audioConfig);
     client.baseUrl = 'wss://api-test.rev.ai/speechtotext/v1alpha/stream';
         
     client.on('close', (code, reason) => {
         console.log(`Connection closed, ${code}: ${reason}`);
+        expect(code).toBe(1006);
+        expect(reason).toBe('Connection dropped by remote peer.');
+        done();
     });
     client.on('httpResponse', code => {
         console.log(code);
@@ -27,33 +30,66 @@ test('Can stream raw audio file', async (done) => {
     var stream = client.start();
 
     stream.on('data', data => {
-        console.log(JSON.stringify(data, null, 2));
+        console.log(data);
+        if(data.type === 'partial') {
+            assertPartialHypothesis(data);
+        } else if (data.type === 'final') {
+            assertFinalHypothesis(data);
+            console.log('final was found');
+            stream.destroy();
+        }
+    });
+
+    stream.on('drain', () => {
+        console.log('drain event triggered')
+    });
+
+    stream.on('close', () => {
+        console.log('close event triggered');
+    });
+
+    stream.on('error', (error) => {
+        console.log(error);
+        console.log('error event triggered')
     });
 
     stream.on('end', () => {
-        console.log('Stream end event called');
+        console.log('stream end was triggered');
+        client.end();
     })
 
-    // var bufs = [];
-    var file = fs.createReadStream('./resources/english_test.raw');
+    stream.on('finish', () => {
+        console.log('finish was called');
+    })
 
-    // file.once('end', () => {
-    //     console.log('File stream end event was triggered');
-    // });
+    var eos = new Buffer('EOS', 'utf8');
 
-    // file.on('data', (chunk) => {
-    //     bufs.push(chunk);
-    // })
+    var file = fs.createReadStream('./resources/english_test.raw', { highWaterMark: 128 * 1024 });
 
-    // file.on('end', () => {
-    //     var buf = Buffer.concat(bufs);
-    //     stream.write(buf);
-    // })
+    file.once('readable', () => {
+        file.pipe(stream);
+    })
 
-    file.on('end', () => {
-        client.end();
-    });
 
-    file.pipe(stream);
-    
 })
+
+function assertPartialHypothesis(partial) {
+    partial.elements.forEach(element => {
+        expect(element.type).toBe('text');
+        expect(element.value).toBeDefined();
+    });
+}
+
+function assertFinalHypothesis(final) {
+    expect(final.ts).toBeLessThan(final.end_ts);
+    final.elements.forEach(element => {
+        if(element.type === 'punct') {
+            expect(element.value).toBeDefined();
+        } else {
+            expect(element.type).toBe('text');
+            expect(element.value).toBeDefined();
+            expect(element.ts).toBeLessThan(element.end_ts);
+            expect(element.confidence).toBe(1);
+        }
+    });
+}
