8ab2d6868ddd7cfbd75f6689cf7ed2ac4fc09fe1
kbridbur <kbridbur@mit.edu>
2019-09-04 12:54:53 -0700

    AISDK-55: Fix CaptionType Export:

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './api-client';
 export * from './streaming-client';
+export * from './models/async/CaptionType';
 export * from './models/async/RevAiAccount';
 export * from './models/async/RevAiJobOptions';
 export * from './models/async/CustomVocabulary';
