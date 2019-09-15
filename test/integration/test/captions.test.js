40983aae2759a2d6d77158fb89d59ac4a76da2f1
kbridbur <kbridbur@mit.edu>
2019-09-15 12:02:53 -0400

    Small integration fixes

--- test/integration/test/captions.test.js
+++ test/integration/test/captions.test.js
@@ -1,12 +1,24 @@
 const clientHelper = require('../src/client-helper');
 const configHelper = require('../src/config-helper');
+const InstanceStateError = require('../../../src/models/RevAiApiError').InvalidStateError;
 
 test('Can get srt captions', async (done) => {
     const client = clientHelper.getClient(configHelper.getApiKey());
     const jobList = await client.getListOfJobs();
     const jobId = clientHelper.getTranscribedJobId(jobList);
     expect(jobId).toBeDefined();
-    const captionsStream = await client.getCaptions(jobId);
+    var captionStream = null;
+    try {
+        captionsStream = await client.getCaptions(jobId);
+    }
+    catch (error){
+        if (error instanceof InvalidStateError){
+            captionsStream = await client.getCaptions(jobId, undefined, 0);
+        }
+        else {
+            throw error;
+        }
+    }
     var streamString = '';
     captionsStream.on('data', data => {
         streamString += data.toString();
