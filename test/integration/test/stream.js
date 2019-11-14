417006babc4e919da169dc3fb1485c1b82203140
Kostas Moustaklem <kostas.moustaklem@rev.com>
2019-11-14 16:21:41 -0600

    Delete Unneeded Space

--- test/integration/test/stream.js
+++ test/integration/test/stream.js
@@ -8,7 +8,7 @@ const assert = require('assert');
     const audioConfig = new AudioConfig("audio/x-raw", 'interleaved', 16000, 'S16LE', 1);
     const client = new RevAiStreamingClient(configHelper.getApiKey(), audioConfig);
     client.baseUrl = `wss://${configHelper.getBaseUrl()}/speechtotext/v1alpha/stream`;
-        
+
     client.on('close', (code, reason) => {
         assertCloseCodeAndReason(code, reason);
         printPassStatement();
