25af7d5b615b6f9c5af988807567552849fb711c
Kodi Shiflett <kodi.shiflett@rev.com>
2021-08-06 09:21:08 -0700

    Updated

--- examples/async_transcribe_local_media_file.js
+++ examples/async_transcribe_local_media_file.js
@@ -15,9 +15,9 @@ const token = require('./config/config.json').access_token;
         metadata: "InternalOrderNumber=123456789",
         callback_url: "https://jsonplaceholder.typicode.com/posts",
         skip_diarization: false,
-        skip_punctuation: false, //Optional value available with some languages
-        speaker_channels_count: null, //Optional value available with some languages
-        custom_vocabularies: [{  //Optional value available with some languages
+        skip_punctuation: false, // Optional value available with some languages
+        speaker_channels_count: null, // Optional value available with some languages
+        custom_vocabularies: [{  // Optional value available with some languages
             phrases: [
                 "add",
                 "custom",
@@ -25,11 +25,10 @@ const token = require('./config/config.json').access_token;
                 "here"
             ]
         }],
-        filter_profanity: false, //Optional value available with some languages
-        remove_disfluencies: false, //Optional value available with some languages
+        filter_profanity: false, // Optional value available with some languages
+        remove_disfluencies: false, // Optional value available with some languages
         delete_after_seconds: 2592000, // 30 days in seconds
-        language: "en" // specify language using the one of the supported ISO 639-1 (2-letter) or
-                       // ISO 639-3(3 - letter) language codes as defined in the API Reference
+        language: "en" // Supported ISO 639-1 (2-letter) or ISO 639-3 (3-letter) language code
     };
 
     // Media may be submitted from a local file
