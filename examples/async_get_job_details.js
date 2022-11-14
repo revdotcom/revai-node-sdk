a8e0428355bb420611a46820681d90862cf646b6
Jenny Wong <jenny.wong@rev.com>
2022-11-14 10:55:04 -0800

    slash

--- examples/async_get_job_details.js
+++ examples/async_get_job_details.js
@@ -10,7 +10,7 @@ const token = require('./config/config.json').access_token;
     console.log(`Account: ${account.email}`);
     console.log(`Credits remaining: ${account.balance_seconds} seconds`);
 
-    var filenames = [".\\resources\\example.mp3", ".\\resources\\example.mp3"];
+    var filenames = ["./resources/example.mp3", "./resources/example.mp3"];
     filenames.map(async filename => await client.submitJobLocalFile(`${filename}`));
 
     /**
